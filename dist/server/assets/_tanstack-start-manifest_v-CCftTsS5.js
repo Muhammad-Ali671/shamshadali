7d66846a357e078ec9d650fcc589a8197cef2a47
Muhammad Ali <muhammadalimirza671@gmail.com>
2026-05-18 11:50:58 +0500

    force push production dist folder

---
+++ dist/server/assets/_tanstack-start-manifest_v-CCftTsS5.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "D:/shamshadali/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-IWNXVPCM.js"] }, "/": { filePath: "D:/shamshadali/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-DaSQMWjP.js"] } }, clientEntry: "/assets/index-IWNXVPCM.js" });
+export {
+  tsrStartManifest
+};
